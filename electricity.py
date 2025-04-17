27873637f43aca171120f5f0300eea6286f8cc97
Veer1578 <sweetyatulshah@gmail.com>
2025-04-17 09:21:58 +0530

    electricity

---
+++ electricity.py
@@ -0,0 +1,17 @@
+units = int(input("Enter no.of units consumed "))
+
+if units < 50:
+    amount = units * 2.60
+    tax = 25
+elif units <= 100:
+    amount = 130 + ((units - 50) * 3.25)
+    tax = 35
+elif units <= 200:
+    amount = 130 + 162.50 + ((units - 100) * 5.26)
+    tax = 45
+else:
+    amount = 130 + 162.50 + 526 + ((units - 200) * 8.45)
+    tax = 75
+
+total = amount + tax
+print(f"Electricity bill is {total}")
